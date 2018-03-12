916c70faed862da6212bf87bab865cbc8c96f36e
Administrator <790168710@qq.com>
2018-03-12 12:14:37 +0800

    项目搭建完成

--- src/main.js
+++ src/main.js
@@ -3,13 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import promise from 'es6-promise'
+import 'babel-polyfill'
 
 Vue.config.productionTip = false
+Vue.config.devtool = true
+//屏蔽所有控制台错误
+Vue.config.errorHandler = (err, vm, info) => {
+}
+
+Vue.prototype.$http = axios
+promise.polyfill()
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
